0f000b4f0d93a7cf8fd2c74592d9031b030db0fa
csc200group7-blip <csc200group7@gmail.com>
2026-01-07 13:52:03 +0100

    Update Invoice.Designer.vb

--- Forms/Invoice.Designer.vb
+++ Forms/Invoice.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class InvoiceForm
     Inherits System.Windows.Forms.Form
 
@@ -17,243 +17,218 @@ Partial Class InvoiceForm
     'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    Friend WithEvents lblTitle As Label
-    Friend WithEvents lblTotal As Label
-    Friend WithEvents InvoiceGrid As DataGridView
-    Friend WithEvents BtnPrint As Button
-
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
-        Dim DataGridViewCellStyle3 As DataGridViewCellStyle = New DataGridViewCellStyle()
         lblTitle = New Label()
-        lblTotal = New Label()
-        BtnPrint = New Button()
         ItemsList = New DataGridView()
-        Panel1 = New Panel()
         IndexCol = New DataGridViewTextBoxColumn()
         ItemCol = New DataGridViewTextBoxColumn()
         QuantityCol = New DataGridViewTextBoxColumn()
         PriceCol = New DataGridViewTextBoxColumn()
-        Total = New DataGridViewTextBoxColumn()
-        Label1 = New Label()
-        CustonerNameLabel = New Label()
-        CustomerAddressLabel = New Label()
-        CustomerEmailLabel = New Label()
+        TotalCol = New DataGridViewTextBoxColumn()
+        btnPrint = New Button()
+        btnExport = New Button()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        lblSubtotal = New Label()
+        lblTax = New Label()
+        lblTotal = New Label()
         CType(ItemsList, ComponentModel.ISupportInitialize).BeginInit()
         Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
         SuspendLayout()
-        ' 
+        '
         ' lblTitle
-        ' 
+        '
         lblTitle.AutoSize = True
-        lblTitle.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTitle.Font = New Font("Segoe UI", 14.0!, FontStyle.Bold)
         lblTitle.Location = New Point(20, 20)
         lblTitle.Name = "lblTitle"
-        lblTitle.Size = New Size(176, 31)
-        lblTitle.TabIndex = 7
+        lblTitle.Size = New Size(180, 32)
         lblTitle.Text = "Invoice Preview"
-        ' 
-        ' lblTotal
-        ' 
-        lblTotal.AutoSize = True
-        lblTotal.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        lblTotal.Location = New Point(13, 428)
-        lblTotal.Name = "lblTotal"
-        lblTotal.Size = New Size(180, 25)
-        lblTotal.TabIndex = 1
-        lblTotal.Text = "Total Amount: ₦0.00"
-        ' 
-        ' BtnPrint
-        ' 
-        BtnPrint.BackColor = Color.FromArgb(CByte(0), CByte(120), CByte(215))
-        BtnPrint.FlatAppearance.BorderSize = 0
-        BtnPrint.FlatStyle = FlatStyle.Flat
-        BtnPrint.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
-        BtnPrint.ForeColor = Color.White
-        BtnPrint.Location = New Point(220, 554)
-        BtnPrint.Name = "BtnPrint"
-        BtnPrint.Size = New Size(575, 40)
-        BtnPrint.TabIndex = 0
-        BtnPrint.Text = "Print / Export Invoice"
-        BtnPrint.UseVisualStyleBackColor = False
-        ' 
+        '
         ' ItemsList
-        ' 
+        '
         ItemsList.AllowUserToAddRows = False
         ItemsList.AllowUserToDeleteRows = False
         ItemsList.BackgroundColor = Color.White
         ItemsList.BorderStyle = BorderStyle.None
         DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle1.BackColor = Color.Gainsboro
-        DataGridViewCellStyle1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        DataGridViewCellStyle1.Font = New Font("Segoe UI", 9.0!, FontStyle.Bold)
         DataGridViewCellStyle1.ForeColor = SystemColors.WindowText
         DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
         DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
         DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
         ItemsList.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
         ItemsList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        ItemsList.Columns.AddRange(New DataGridViewColumn() {IndexCol, ItemCol, QuantityCol, PriceCol, Total})
+        ItemsList.Columns.AddRange(New DataGridViewColumn() {IndexCol, ItemCol, QuantityCol, PriceCol, TotalCol})
         DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle2.BackColor = SystemColors.Window
-        DataGridViewCellStyle2.Font = New Font("Segoe UI", 9F)
+        DataGridViewCellStyle2.Font = New Font("Segoe UI", 9.0!)
         DataGridViewCellStyle2.ForeColor = SystemColors.ControlText
         DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
         DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
         DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
         ItemsList.DefaultCellStyle = DataGridViewCellStyle2
         ItemsList.EnableHeadersVisualStyles = False
-        ItemsList.Location = New Point(3, 151)
-        ItemsList.MaximumSize = New Size(1500, 0)
+        ItemsList.Location = New Point(20, 70)
         ItemsList.Name = "ItemsList"
         ItemsList.ReadOnly = True
-        DataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle3.BackColor = SystemColors.Control
-        DataGridViewCellStyle3.Font = New Font("Segoe UI", 9F)
-        DataGridViewCellStyle3.ForeColor = SystemColors.WindowText
-        DataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight
-        DataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText
-        DataGridViewCellStyle3.WrapMode = DataGridViewTriState.True
-        ItemsList.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
         ItemsList.RowHeadersVisible = False
         ItemsList.RowHeadersWidth = 51
-        ItemsList.Size = New Size(1066, 262)
-        ItemsList.TabIndex = 9
-        ' 
-        ' Panel1
-        ' 
-        Panel1.BackColor = Color.GhostWhite
-        Panel1.Controls.Add(CustomerEmailLabel)
-        Panel1.Controls.Add(CustomerAddressLabel)
-        Panel1.Controls.Add(CustonerNameLabel)
-        Panel1.Controls.Add(Label1)
-        Panel1.Controls.Add(ItemsList)
-        Panel1.Controls.Add(lblTotal)
-        Panel1.Location = New Point(-1, 54)
-        Panel1.Name = "Panel1"
-        Panel1.Size = New Size(1094, 465)
-        Panel1.TabIndex = 6
-        ' 
+        ItemsList.Size = New Size(740, 300)
+        '
         ' IndexCol
-        ' 
-        IndexCol.FillWeight = 30.03003F
+        '
         IndexCol.HeaderText = "S/N"
         IndexCol.MinimumWidth = 30
         IndexCol.Name = "IndexCol"
         IndexCol.ReadOnly = True
-        IndexCol.Width = 60
-        ' 
+        IndexCol.Width = 50
+        '
         ' ItemCol
-        ' 
-        ItemCol.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
-        ItemCol.FillWeight = 303.1112F
+        '
         ItemCol.HeaderText = "Item"
-        ItemCol.MinimumWidth = 176
+        ItemCol.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
         ItemCol.Name = "ItemCol"
         ItemCol.ReadOnly = True
-        ' 
+        '
         ' QuantityCol
-        ' 
-        QuantityCol.FillWeight = 43.3671455F
+        '
         QuantityCol.HeaderText = "Quantity"
-        QuantityCol.MinimumWidth = 55
+        QuantityCol.MinimumWidth = 60
         QuantityCol.Name = "QuantityCol"
         QuantityCol.ReadOnly = True
-        QuantityCol.Width = 86
-        ' 
+        QuantityCol.Width = 80
+        '
         ' PriceCol
-        ' 
-        PriceCol.FillWeight = 60.1990433F
+        '
         PriceCol.HeaderText = "Price"
-        PriceCol.MinimumWidth = 65
+        PriceCol.MinimumWidth = 80
         PriceCol.Name = "PriceCol"
         PriceCol.ReadOnly = True
-        PriceCol.Width = 121
-        ' 
-        ' Total
-        ' 
-        Total.HeaderText = "Total"
-        Total.MinimumWidth = 6
-        Total.Name = "Total"
-        Total.ReadOnly = True
-        Total.Width = 155
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI Semibold", 11.8F, FontStyle.Bold)
-        Label1.Location = New Point(9, 13)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(71, 28)
-        Label1.TabIndex = 10
-        Label1.Text = "Bill To:"
-        ' 
-        ' CustonerNameLabel
-        ' 
-        CustonerNameLabel.AutoSize = True
-        CustonerNameLabel.Font = New Font("Segoe UI Semibold", 10F)
-        CustonerNameLabel.Location = New Point(13, 45)
-        CustonerNameLabel.Name = "CustonerNameLabel"
-        CustonerNameLabel.Size = New Size(95, 23)
-        CustonerNameLabel.TabIndex = 11
-        CustonerNameLabel.Text = "Max Essien"
-        ' 
-        ' CustomerAddressLabel
-        ' 
-        CustomerAddressLabel.AutoSize = True
-        CustomerAddressLabel.Font = New Font("Segoe UI Semibold", 9.5F)
-        CustomerAddressLabel.Location = New Point(13, 95)
-        CustomerAddressLabel.Name = "CustomerAddressLabel"
-        CustomerAddressLabel.Size = New Size(218, 21)
-        CustomerAddressLabel.TabIndex = 12
-        CustomerAddressLabel.Text = "123 Main Str, Surulere, Lagos"
-        ' 
-        ' CustomerEmailLabel
-        ' 
-        CustomerEmailLabel.AutoSize = True
-        CustomerEmailLabel.Font = New Font("Segoe UI Semibold", 9.7F)
-        CustomerEmailLabel.Location = New Point(13, 68)
-        CustomerEmailLabel.Name = "CustomerEmailLabel"
-        CustomerEmailLabel.Size = New Size(138, 23)
-        CustomerEmailLabel.TabIndex = 13
-        CustomerEmailLabel.Text = "max@gmail.com"
-        ' 
+        PriceCol.Width = 100
+        '
+        ' TotalCol
+        '
+        TotalCol.HeaderText = "Total"
+        TotalCol.MinimumWidth = 80
+        TotalCol.Name = "TotalCol"
+        TotalCol.ReadOnly = True
+        TotalCol.Width = 120
+        '
+        ' Panel1 (Totals section)
+        '
+        Panel1.Controls.Add(lblSubtotal)
+        Panel1.Controls.Add(lblTax)
+        Panel1.Controls.Add(lblTotal)
+        Panel1.Location = New Point(500, 380)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(260, 120)
+        '
+        ' lblSubtotal
+        '
+        lblSubtotal.AutoSize = True
+        lblSubtotal.Font = New Font("Segoe UI", 10.0!, FontStyle.Regular)
+        lblSubtotal.Location = New Point(10, 10)
+        lblSubtotal.Name = "lblSubtotal"
+        lblSubtotal.Text = "Subtotal: $0.00"
+        '
+        ' lblTax
+        '
+        lblTax.AutoSize = True
+        lblTax.Font = New Font("Segoe UI", 10.0!, FontStyle.Regular)
+        lblTax.Location = New Point(10, 40)
+        lblTax.Name = "lblTax"
+        lblTax.Text = "Tax: $0.00"
+        '
+        ' lblTotal
+        '
+        lblTotal.AutoSize = True
+        lblTotal.Font = New Font("Segoe UI", 12.0!, FontStyle.Bold)
+        lblTotal.Location = New Point(10, 70)
+        lblTotal.Name = "lblTotal"
+        lblTotal.Text = "Total: $0.00"
+        '
+        ' btnPrint
+        '
+        btnPrint.BackColor = Color.FromArgb(0, 120, 215)
+        btnPrint.ForeColor = Color.White
+        btnPrint.FlatStyle = FlatStyle.Flat
+        btnPrint.FlatAppearance.BorderSize = 0
+        btnPrint.Location = New Point(20, 520)
+        btnPrint.Name = "btnPrint"
+        btnPrint.Size = New Size(120, 40)
+        btnPrint.Text = "Print"
+        btnPrint.UseVisualStyleBackColor = False
+        '
+        ' btnExport
+        '
+        btnExport.BackColor = Color.FromArgb(0, 120, 215)
+        btnExport.ForeColor = Color.White
+        btnExport.FlatStyle = FlatStyle.Flat
+        btnExport.FlatAppearance.BorderSize = 0
+        btnExport.Location = New Point(160, 520)
+        btnExport.Name = "btnExport"
+        btnExport.Size = New Size(120, 40)
+        btnExport.Text = "Export"
+        btnExport.UseVisualStyleBackColor = False
+        '
+        ' Panel2 (Main container)
+        '
+        Panel2.Controls.Add(lblTitle)
+        Panel2.Controls.Add(ItemsList)
+        Panel2.Controls.Add(Panel1)
+        Panel2.Controls.Add(btnPrint)
+        Panel2.Controls.Add(btnExport)
+        Panel2.Dock = DockStyle.Fill
+        Panel2.Name = "Panel2"
+        Panel2.BackColor = Color.White
+        '
+        ' Panel3 (Optional: Customer Info)
+        '
+        Panel3.Location = New Point(20, 400)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(460, 100)
+        '
         ' InvoiceForm
-        ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        '
+        AutoScaleDimensions = New SizeF(8.0!, 20.0!)
         AutoScaleMode = AutoScaleMode.Font
-        AutoScroll = True
-        BackColor = Color.White
-        ClientSize = New Size(1091, 630)
-        Controls.Add(Panel1)
-        Controls.Add(BtnPrint)
-        Controls.Add(lblTitle)
-        Font = New Font("Segoe UI", 9F)
+        ClientSize = New Size(800, 580)
+        Controls.Add(Panel2)
         Name = "InvoiceForm"
-        Text = "Invoice"
+        Text = "Invoice Preview"
+        BackColor = Color.White
         CType(ItemsList, ComponentModel.ISupportInitialize).EndInit()
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel3.ResumeLayout(False)
         ResumeLayout(False)
-        PerformLayout()
-
     End Sub
-    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
-    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+
+    Friend WithEvents lblTitle As Label
     Friend WithEvents ItemsList As DataGridView
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Total As DataGridViewTextBoxColumn
     Friend WithEvents IndexCol As DataGridViewTextBoxColumn
     Friend WithEvents ItemCol As DataGridViewTextBoxColumn
     Friend WithEvents QuantityCol As DataGridViewTextBoxColumn
     Friend WithEvents PriceCol As DataGridViewTextBoxColumn
-    Friend WithEvents Label1 As Label
-    Friend WithEvents CustomerAddressLabel As Label
-    Friend WithEvents CustonerNameLabel As Label
-    Friend WithEvents CustomerEmailLabel As Label
-
+    Friend WithEvents TotalCol As DataGridViewTextBoxColumn
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnExport As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblTotal As Label
 End Class
